ad4394c73cd3b4bcfc1f70a0e47a150d7f77ce5f
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-03 13:50:23 +0300

    API requests Added

---
+++ src/API/Request.js
@@ -0,0 +1,22 @@
+const request = {
+  fetchParis:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=48.8566&lon=2.3522&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchLondon:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=51.5074&lon=0.1278&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchLisbon:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=38.7223&lon=9.1393&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchMadrid:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=40.4168&lon=3.7038&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchRome:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=41.9028&lon=12.4964&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchBarcelona:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=41.3851&lon=2.1734&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchWarsaw:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=52.2297&lon=21.0122&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchBrussels:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=50.8503&lon=4.3517&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+  fetchEurope:
+    "https://api.openweathermap.org/data/2.5/air_pollution?lat=54.5260&lon=15.2551&appid=f1d8a4d595c7a03c5224b9f44ae09d6d",
+};
+
+export default request;
