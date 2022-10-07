466c37270ed6ffbcbd50187a56905b6e809f13dd
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-07 21:43:30 +0300

    Use Route for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import ItemScreen from "./screens/ItemScreen";
 import Home from "./screens/Home";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<ItemScreen />} />
+      </Routes>
     </div>
   );
 }
