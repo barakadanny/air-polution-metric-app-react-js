d5c20b9828c7623be157d1de914508fc866b9a59
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-07 21:15:06 +0300

    Add icons and animations

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+import { BsFillGeoFill } from "react-icons/bs";
 import "./item.css";
 
 function Item() {
@@ -8,10 +10,14 @@ function Item() {
       <div className="item__content">
         <div className="item__title">
           <h3>PARIS</h3>
+          <AiOutlineDoubleRight className="item__icon" />
         </div>
         <div className="item__description">
-          <span>2.3522N</span> <br />
-          <span>2.3522N</span>
+          <div className="">
+            <span>2.3522N</span> <br />
+            <span>2.3522N</span>
+          </div>
+          <BsFillGeoFill className="item__icon" />
         </div>
       </div>
     </div>
