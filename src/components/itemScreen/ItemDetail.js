488574f082df4e788e79c4a97ef46c7348eba7a8
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-07 22:23:38 +0300

    ItemDetail styled

--- src/components/itemScreen/ItemDetail.js
+++ src/components/itemScreen/ItemDetail.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./ItemDetail.css";
 
 function ItemDetail() {
-  return <div className="item__detail">ItemDetail</div>;
+  return (
+    <div className="item__detail">
+      <div className="item__detail__content">
+        <div className="item__detail__title">NH3:</div>
+        <div className="item__detail__description">188.59</div>
+      </div>
+      <div className="item__detail__content">
+        <div className="item__detail__title">NO2:</div>
+        <div className="item__detail__description">0.46</div>
+      </div>
+      <div className="item__detail__content">
+        <div className="item__detail__title">O3:</div>
+        <div className="item__detail__description">16.11</div>
+      </div>
+    </div>
+  );
 }
 
 export default ItemDetail;
