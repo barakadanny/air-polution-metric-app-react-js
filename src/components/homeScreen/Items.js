b041b8b20aa32a31e0f3f8494f0ed0f0163f9bce
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-10 22:10:00 +0300

    Input search Added

--- src/components/homeScreen/Items.js
+++ src/components/homeScreen/Items.js
@@ -6,15 +6,22 @@ import "./Items.css";
 function Items() {
   return (
     <div className="items">
-      {placesData.map((place) => (
-        <Item
-          key={place.id}
-          id={place.id}
-          name={place.name}
-          lat={place.lat}
-          long={place.long}
-        />
-      ))}
+      {/* input search */}
+      <div className="items__search">
+        <input className="search__input" type="text" placeholder="Search" />
+      </div>
+      {/* items */}
+      <div className="items__container">
+        {placesData.map((place) => (
+          <Item
+            key={place.id}
+            id={place.id}
+            name={place.name}
+            lat={place.lat}
+            long={place.long}
+          />
+        ))}
+      </div>
     </div>
   );
 }
