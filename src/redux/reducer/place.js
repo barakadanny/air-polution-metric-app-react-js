cba42b0c5366dc8f33aa49fd2c6c0745cdfc6b2e
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-12 16:33:02 +0300

    Fix responsiveness

--- src/redux/reducer/place.js
+++ src/redux/reducer/place.js
@@ -5,12 +5,10 @@ const API_KEY = 'f1d8a4d595c7a03c5224b9f44ae09d6d';
 const LOAD_DATA = 'place/LOAD_DATA';
 
 const fetchData = createAsyncThunk(LOAD_DATA, async (payload) => {
-  console.log(payload);
   const response = await fetch(
     `${BASE_URL}lat=${payload.lat}&lon=${payload.long}&appid=${API_KEY}`,
   );
   const { list } = await response.json();
-  console.log(list[0].components);
   return list[0].components;
 });
 
