9406ebd83e1e8a109be87e54610062af0901e42b
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-10-12 19:51:47 +0300

    Mock the store

---
+++ src/__mocks__/store.js
@@ -0,0 +1,17 @@
+function store() {
+  return [
+    {
+      coord: { lat: 121, lon: 3423 },
+      list: {
+        co: 1212,
+        so2: 1212121,
+        nho3: 2323,
+        o3: 5454,
+        PM2_5: 23323,
+        PM10: 243,
+      },
+    },
+  ];
+}
+
+export default store;
